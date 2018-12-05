c94e1d05fe051f9223d94ac9ac7b88342682382d
tomef96 <tomef96@gmail.com>
2018-12-05 10:31:42 +0100

    init iOS

---
+++ iOS/smidig/smidig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smidig
+//
+//  Created by Tom Fevang on 05/12/2018.
+//  Copyright © 2018 Tom Fevang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
