12926af61833f9758362a853512e23774b393d75
tomef96 <tomef96@gmail.com>
2018-12-10 11:32:34 +0100

    Event files

---
+++ iOS/smidig/Event.swift
@@ -0,0 +1,20 @@
+//
+//  Event.swift
+//  smidig
+//
+//  Created by Tom Fevang on 10/12/2018.
+//  Copyright © 2018 Tom Fevang. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    
+    var author: String
+    var places: Int
+    var title: String
+    var description: String
+    var image: String
+    var isJoined: Bool
+    
+}
