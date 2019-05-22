c1821f142bd23ee922d33ed6de65041bb400f09e
Jan-Kristian Evjen <evjen@Jan-Kristians-MacBook-Pro.local>
2019-05-22 14:27:53 +0200

    changes background color of chat bubble

--- iOS/smidig/smidig/Main/Chat/MessageTableViewController.swift
+++ iOS/smidig/smidig/Main/Chat/MessageTableViewController.swift
@@ -85,6 +85,10 @@ class MessageTableViewController: UITableViewController {
         cell.messageLabel.layer.cornerRadius = 10
         cell.messageLabel.layer.masksToBounds = true
         
+        if (Auth.auth().currentUser?.displayName == cell.message?.author) {
+            
+        }
+        
         if (previous != nil) {
             if (previous?.author == entry.author) {
                 cell.authorLabel.text = " "
