3f806eb0ecfef458ae323989bd443851fdffb8fb
tomef96 <tomef96@gmail.com>
2019-06-07 14:26:43 +0200

    new feed cell

--- iOS/smidig/smidig/Onboarding/Register/RegisterViewController.swift
+++ iOS/smidig/smidig/Onboarding/Register/RegisterViewController.swift
@@ -27,6 +27,9 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     @objc
@@ -139,4 +142,17 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 }
